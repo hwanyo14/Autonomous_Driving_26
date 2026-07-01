43afe91e6279c97f6064dcf6408f8e5aaed26c07
KSH3880 <montm3880@gmail.com>
2026-07-01 17:02:59 +0900

    tmp

--- train_total_3.sh
+++ train_total_3.sh
@@ -2,9 +2,10 @@
 
 set -euo pipefail
 
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 \
+CUDA_VISIBLE_DEVICES=2,3,4,5,6,7,0,1 \
 PORT=25003 \
 USE_MPS=0 \
+PYTORCH_CUDA_ALLOC_CONF=expandable_segments:True \
 ./run.sh \
 ./projects/configs/baselines/shape_guide_128_dice_weight_full.py \
 8 \
